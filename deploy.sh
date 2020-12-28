fc9048cb7af94639f9ace031afb8360063bfbddb
Alexander Grebennikov <mollyfox6996@gmail.com>
2020-12-28 17:04:00 +0300

    Fix email host

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 
-IMAGE="alexgrebennikov/t-shirt_backend"
+
 docker build -t alexgrebennikov/t-shirt_backend .
 
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 docker push alexgrebennikov/t-shirt_backend:latest
 
 docker stop sample_api_1
-docker run --restart unless-stopped -d -n sample_api_1 -p 5000:80 alexgrebennikov/t-shirt_backend
+docker run --restart unless-stopped -d  sample_api_1 -p 5000:80 alexgrebennikov/t-shirt_backend
 docker system prune -a -f
