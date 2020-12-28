ac1bc6eed6bdfb72c4c7fa2c62dd261b1cb936f5
Alexander Grebennikov <mollyfox6996@gmail.com>
2020-12-28 18:31:05 +0300

    .

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,3 @@ docker build -t alexgrebennikov/t-shirt_backend .
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 docker push alexgrebennikov/t-shirt_backend:latest
 
-docker stop sample_api_1
-docker run --restart unless-stopped -d  sample_api_1 -p 5000:80 alexgrebennikov/t-shirt_backend
-docker system prune -a -f
