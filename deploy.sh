7cd0df3507200dc14769334ede62e51f4558d780
mollyfox6996 <56237792+mollyfox6996@users.noreply.github.com>
2020-12-31 21:58:05 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,8 @@ docker push alexgrebennikov/t-shirt_backend:latest
 ssh -i ./deployf_rsa root@165.227.158.125 <<EOF
 
     docker stop api
-    docker rm api
+    docker rm api 
+    docker restart db
     docker run -d --name api -p 5000:80 --restart always alexgrebennikov/t-shirt_backend
     docker system prune -a -f
 EOF
