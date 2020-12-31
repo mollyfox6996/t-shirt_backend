fc06bd18528ea9b1a023aac2fe5832a1a4ee1840
mollyfox6996 <56237792+mollyfox6996@users.noreply.github.com>
2020-12-31 22:22:08 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdi
 docker push alexgrebennikov/t-shirt_backend:latest
 
 ssh -i ./deployf_rsa root@165.227.158.125 <<EOF
-    cd /var/www/tshirt
+    cd /var/www/t-shirt
     docker stop api
     docker system prune -a -f
     docker-compose stop
