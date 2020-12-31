ffb679d3c4cc64b83dad8fb02620ae9b1d007c96
Alexander Grebennikov <mollyfox6996@gmail.com>
2020-12-31 19:11:56 +0300

    merge

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,12 @@ echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdi
 docker push alexgrebennikov/t-shirt_backend:latest
 
 
+
+
+ssh root@165.227.158.125<<EOF
+
+    docker stop t-shirt_api_1
+    docker run -d --name t-shirt_api_1 -p 5000:80 --restart always alexgrebennikov/t-shirt_backend
+    docker system prune -a -f
+EOF
+
