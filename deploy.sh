79171078e0d51a8c1793373012ef386eaeeb5893
mollyfox6996 <56237792+mollyfox6996@users.noreply.github.com>
2021-01-17 18:51:20 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ docker build -t alexgrebennikov/t-shirt_backend .
 echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin
 docker push alexgrebennikov/t-shirt_backend:latest
 
-cd $HOME 
+cd - 
 ssh -i ./deployf_rsa root@165.227.158.125 <<EOF
     cd /var/www/t-shirt
     docker stop api
